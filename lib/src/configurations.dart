23a74a64a358e570429c6975641638d7cb66b5f5
Waqas Arshad <waqas.arshad@jintech.com>
2023-11-13 12:43:14 +0500

    Modified typeahead dependency

--- lib/src/configurations.dart
+++ lib/src/configurations.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:flutter_typeahead/flutter_typeahead2.dart';
 
 ///
 class SuggestionConfiguration {
